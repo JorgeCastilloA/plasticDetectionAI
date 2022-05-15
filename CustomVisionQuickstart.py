51805d03aa39d207d893f4b1c2034cc859139436
JorgeCastilloA <99519741+JorgeCastilloA@users.noreply.github.com>
2022-05-15 15:01:45 -0500

    Add: File correction

--- CustomVisionQuickstart.py
+++ CustomVisionQuickstart.py
@@ -1,6 +1,3 @@
-# pip install --upgrade azure-cognitiveservices-vision-computervision in cmd (as Admin) or Powershell
-from azure.cognitiveservices.vision.customvision.prediction import CustomVisionPredictionClient
-from msrest.authentication import ApiKeyCredentials
 from turtle import width
 from unittest import result
 # install pip install Pillow in cmd (as Admin) or Powershell
@@ -8,15 +5,6 @@ from matplotlib.image import imread
 from matplotlib.patches import Rectangle
 from matplotlib import pyplot as plt
 
-
-prediction_key = '8042c8d7a4884dfa99765088d1f7f075'
-ENDPOINT = 'https://southcentralus.api.cognitive.microsoft.com/'
-project_id = 'b84345e6-5e7f-440e-b330-14e2ddcd59af'
-publish_iteration_name = 'Iteration2'
-
-prediction_credentials = ApiKeyCredentials(
-    in_headers={"Prediction-key": prediction_key})
-predictor = CustomVisionPredictionClient(ENDPOINT, prediction_credentials)
 
 # Image directory
 img_dir = 'INSERT YOUR LOCAL DIR WITH IMAGE NAME'
